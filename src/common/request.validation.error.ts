d8b5d348b1587762217e85d839d98c6036685ad8
nabeelktr <nabee446@gmail.com>
2024-03-27 20:20:29 +0530

    added validation errors

---
+++ src/common/request.validation.error.ts
@@ -0,0 +1,23 @@
+import { ValidationError } from "express-validator";
+import { CustomError } from "../base/custom.error";
+
+export class RequestValidationError extends CustomError{
+    statusCode: 400;
+
+    constructor(public errors: ValidationError[]){
+        super("Invalid Login Parameters.")
+        Object.setPrototypeOf(this, RequestValidationError.prototype)
+    }
+
+    serializeErrors(): { message: string; field?: string | undefined; }[] {
+        const errorsArray = this.errors.map((error) => {
+            if(error.type === "field"){
+                return {message: error.msg, field: error.path}
+            }
+            return {message: error.msg};
+        });
+
+        return errorsArray;
+    }
+    
+}
