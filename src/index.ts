cecf6752c20c9ee9611354de67b1b3fa67132433
nabeelktr <nabee446@gmail.com>
2024-03-27 20:23:49 +0530

    configured index.ts as main

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export * from "./base/custom.error"
+
+export * from "./common/bad.request"
+export * from "./common/db.connection"
+export * from "./common/not.authorized"
+export * from "./common/not.found"
+export * from "./common/request.validation.error"
